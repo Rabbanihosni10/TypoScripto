41fd7e708bf9b3c8dce6a872cf2c0be7d696863f
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-07-25 16:27:52 +0600

    Basic first 40 min

---
+++ Basic TS/main.js
@@ -0,0 +1,25 @@
+"use strict";
+// const firstName='Hosni Rabbani'; 
+// firstName='Ashraful';
+// console.log(firstName);
+//export {};  //Will not be used always or usually
+Object.defineProperty(exports, "__esModule", { value: true });
+var fullName = "Ashraful Haque";
+var age = 50;
+var skill = "MERN Stack"; //Explicit Type
+var isSenior = false; //Explicit Type
+var contest = 23.33; //Implicit Type
+var sunday = 'Rofiq'; // Implicit Type
+// age='Tobarak';
+var devs;
+devs = 'Rabul';
+// devs=false;
+var raqul;
+raqul = 22;
+console.log(raqul);
+console.log(devs);
+console.log(fullName);
+// any
+var noor;
+noor = 1000;
+console.log(noor);
---
+++ Basic TS/main.ts
@@ -0,0 +1,27 @@
+// const firstName='Hosni Rabbani'; 
+// firstName='Ashraful';
+// console.log(firstName);
+//export {};  //Will not be used always or usually
+
+let fullName:string="Ashraful Haque";
+let age:number=50;
+let skill:string="MERN Stack";  //Explicit Type
+let isSenior:boolean=false;     //Explicit Type
+let contest=23.33;      //Implicit Type
+let sunday='Rofiq';     // Implicit Type
+// age='Tobarak';
+let devs:string;
+devs='Rabul';
+// devs=false;
+let raqul:any;
+raqul=22;
+console.log(raqul);
+console.log(devs);
+console.log(fullName);
+
+// any datatype
+let noor:string | number;
+noor=1000;
+console.log(noor);
+export {};
+ 
