ac49b072d5dcedb08347bc516dde10525ca0b835
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-07-27 04:32:27 +0600

    protected-private-public-1

--- Basic-TS/public/js/main.js
+++ Basic-TS/public/js/main.js
@@ -5,6 +5,22 @@ class Student {
         this.age = age;
         this.skill = skill;
     }
+    agecal() {
+        return `I am from public`;
+    }
+}
+class Teacher extends Student {
+    constructor(name, age, skill) {
+        super(name, age, skill);
+    }
+    ageCal() {
+        return this.name;
+    }
+}
+class Account extends Student {
+    constructor(name, age, skill) {
+        super(name, age, skill);
+    }
 }
 const student = new Student("Ashraf", 30, true);
-console.log(student);
+console.log(student.agecal());
