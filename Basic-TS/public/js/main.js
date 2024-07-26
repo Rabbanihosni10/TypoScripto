a42c3ef718ff6623f970073f27b6625e9ed785d9
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-07-27 02:14:57 +0600

    object, array, type narrowing, tuple

--- Basic-TS/public/js/main.js
+++ Basic-TS/public/js/main.js
@@ -1,19 +1,7 @@
 "use strict";
-// const ageCal=(name: string="Asraf",age: number=30):void /*: string*/=>{
-//     // return `Hi ${name}, you are ${age} years old`;
-//     // return age; will show error that the expected return value is string not number
-//     // return name;
-//     console.log(`Hi ${name}`);
-// };
-// ageCal("Ashraful Haque",30);
-// //if nothing return from function then void will be used in a function perametter type
-// const info = (name: string): void =>{
-//     console.log("");
-// };
-const ageCal = (name = "Asraf", age = 30) => {
-    console.log(`Hi ${name}`);
-    // return name;
-};
-const info = (name) => {
-    throw new Error("");
+const info = (val) => {
+    if (typeof val === "string") {
+        return val.toUpperCase();
+    }
+    return val + 10;
 };
