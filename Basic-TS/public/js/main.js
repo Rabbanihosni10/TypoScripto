3851fbb4ddd03d22e10c17764111aee0ba4d45a7
Rabbanihosni10 <rabbanihosni10@gmail.com>
2024-07-27 04:56:57 +0600

    interface

--- Basic-TS/public/js/main.js
+++ Basic-TS/public/js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+class Student {
+    constructor() {
+        this.name = "";
+        this.age = 12;
+        this.skill = true;
+    }
+    ageCal() {
+    }
+    getDevs() {
+        return true;
+    }
+    learnDevs() {
+        return "";
+    }
+}
